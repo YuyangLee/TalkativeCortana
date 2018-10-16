0fc52d273638b189e336f4d0d2fe2ad3aaa16041
AP0STR0PH3 <yuyanglee2002@outlook.com>
2018-10-16 23:58:37 +0800

    10-16 Update 01
    Automatic random line sellector added.

--- Talkative Cortana/Program.cs
+++ Talkative Cortana/Program.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Diagnostics;
 using Microsoft.Win32;
+using System.IO;
 
 namespace Talkative_Cortana
 {
     class Program
     {
         /// <summary>
-        /// Set values in registry and return the result.
+        /// 在注册表中应用设置并返回设置结果（是否成功）。
         /// </summary>
         /// <param name="Content"></param>
         /// <returns></returns>
@@ -37,15 +38,15 @@ namespace Talkative_Cortana
             }
             return Res;
         }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input words U want Cortana 2 tell.");
-            string Content = Console.ReadLine();
-            //Under designing to be able to get words from other sourse, maybe automatically.
-            Console.WriteLine("Finished！\n\nApplying...");
+            Console.WriteLine("请务必先阅读User Guide！\n正在读取txt词条文件。扫描路径：C:\\TCTexts\\Texts.txt");
+            string Content = getRandomLine();
+            Console.WriteLine("\n正在应用设置……");
             if (FixReg(Content))
             {
-                Console.WriteLine("\nFinished!\n\nInput Y 2 refresh explorer.exe or any other word(s) 2 close without refreshing...");
+                Console.WriteLine("\n完成！输入Y来重启explorer.exe进程，输入其他文字以退出。感谢使用！");
                 bool RefreshOrNot = Console.ReadLine().ToUpper() == "Y" ? true : false;
                 if (RefreshOrNot)
                 {
@@ -62,12 +63,33 @@ namespace Talkative_Cortana
                     //Under designing to apply a better approach to refreshing without bothering users.
                     LogoffCMD.Close();
                     Process.Start(Environment.SystemDirectory + @"\..\explorer.exe");
+                    return;
                 }
-                else return;
+                else
+                {
+                    Console.WriteLine("\n您需要手动重启explorer.exe或注销登陆以更新搜索栏。");
+                    return;
+                }
             }
-            else Console.WriteLine("Failed 2 set values. Please check Ur permission settings.\nPress any K 2 exit.");
-            Console.WriteLine("\nDone. Press any K 2 exit! TKS 4 using!");
-            Console.Read();
+            else
+            {
+                Console.WriteLine("\n注册表更新异常，请检查您的系统及权限设置。");
+                return;
+            }
+        }
+
+        static public string getRandomLine()
+        {
+            string Path = @"C:\TCTexts\Texts.txt", ErrorWords = "请检查C:\\TCTexts目录下的词条文本文档。", Input = "";
+            if (!File.Exists(Path)) return ErrorWords;
+            StreamReader txtReader = new StreamReader(Path, Encoding.UTF8);
+            List<string> Lines = new List<string>();
+            while ((Input = txtReader.ReadLine()) != null)
+            {
+                Lines.Add(Input);
+            }
+            Random getRandomLine = new Random();
+            return Lines.Count == 0 ? ErrorWords : Lines[getRandomLine.Next(0, Lines.Count)];
         }
     }
 }
